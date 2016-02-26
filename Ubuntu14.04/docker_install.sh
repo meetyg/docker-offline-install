5655e1dfa3ed0780971ef90d400dd73bd3a7d29b
meetyg <aagamli@gmail.com>
2016-02-26 08:12:09 +0200

    Initial install implementation, not tested

---
+++ Ubuntu14.04/docker_install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Create a directory for mirrored repositories
+mkdir -p /var/deb/apt-mirror
+
+# Extract mirrored repository
+tar -xvzf docker_mirror.tar.gz /var/deb/apt-mirror/
+
+# Add local Docker mirror to apt-get sources list
+echo 'deb [arch=arm64] file:///var/deb/apt-mirror/apt.dockerproject.org/repo  ubuntu-trusty main' > /etc/apt/source.list.d/docker_local.list
+
+# Add the Docker public key to apt-get so that it can verify the package signitures
+apt-key add /var/deb/apt-mirror/apt.dockerproject.org/docker.key
+
+# Run apt-get update so that apt will recognize new local repo added in previous step
+apt-get update
+
+# Install Docker engine
+apt-get install docker-engine
+
