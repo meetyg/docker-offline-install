ef341b862ee84ffa4315f74ce286ced62f5dc996
meetyg <aagamli@gmail.com>
2016-02-26 07:51:45 +0200

    remove sudo stuff from script

--- Ubuntu14.04/docker-download.sh
+++ Ubuntu14.04/docker-download.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Run the follwing command with su privileges
-sudo -i
-
 # Update apt, install apt secure transport and cerificates, and apt-mirror 
 apt-get update
 apt-get -y install apt-transport-https ca-certificates apt-mirror
@@ -19,11 +16,8 @@ apt-mirror
 # Save Docker's public key to the mirror directory
 apt-key export 58118E89F3A912897C070ADBF76221572C52609D > /var/spool/apt-mirror/mirror/apt.dockerproject.org/docker.key
 
-# Return to user
-exit
-
 cd ~
 
-# Compress the mirror dir to an archive file in the current directory
+# Compress the mirror dir to an archive file in the home directory
 tar -cvzf docker_mirror.tar.gz -C /var/spool/apt-mirror/mirror apt.dockerproject.org
 
