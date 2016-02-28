eff3a19756c592c4641d2bba7972e4dfd44bc4e3
meetyg <aagamli@gmail.com>
2016-02-28 13:37:50 +0200

    Fix architecture

--- Ubuntu14.04/docker-install.sh
+++ Ubuntu14.04/docker-install.sh
@@ -7,7 +7,7 @@ mkdir -p /var/deb/apt-mirror
 tar -xvzf docker_mirror.tar.gz -C /var/deb/apt-mirror/
 
 # Add local Docker mirror to apt-get sources list
-echo 'deb [arch=arm64] file:///var/deb/apt-mirror/apt.dockerproject.org/repo  ubuntu-trusty main' > /etc/apt/sources.list.d/docker_local.list
+echo 'deb [arch=amd64] file:///var/deb/apt-mirror/apt.dockerproject.org/repo  ubuntu-trusty main' > /etc/apt/sources.list.d/docker_local.list
 
 # Add the Docker public key to apt-get so that it can verify the package signitures
 apt-key add /var/deb/apt-mirror/apt.dockerproject.org/docker.key
